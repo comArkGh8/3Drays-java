8e89f445dcd54ddfc644c236009d12ac2ad6cee6
comarkgh8 <comarkgh8@gmail.com>
2018-05-19 18:13:39 +0200

    maxDepth from file added

--- src/rays/Main.java
+++ src/rays/Main.java
@@ -10,17 +10,22 @@ import static java.lang.System.out;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        // set constants
-        int maxDepth = 1;
-        float error = 0.00001f;
-        GlobalConstants myConstants = new GlobalConstants(error, maxDepth);
-        
-        
+  
         // generate scene
         Optional<File> file = Optional.empty();
         file = Optional.of(new File(args[0]));
         File fileToInsert = file.get();
         Scene fileScene = new Scene(fileToInsert);
+        
+        int maxDepth = fileScene.maxDepth;
+        if (maxDepth==0) {
+            maxDepth = 1;
+        }
+        
+        
+        // set constants
+        float error = 0.00001f;
+        GlobalConstants myConstants = new GlobalConstants(error, maxDepth);
         
         File outFile = new File(args[1]);
         int numThreads = Integer.parseInt(args[2]);
