7e3b3d10da10e3108f45658adfe08cea04b88d1c
comarkgh8 <comarkgh8@gmail.com>
2018-05-22 09:00:02 +0200

    main diff

--- src/rays/Main.java
+++ src/rays/Main.java
@@ -10,20 +10,27 @@ import static java.lang.System.out;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        // set constants
-        int maxDepth = 1;
-        float error = 0.00001f;
-        GlobalConstants myConstants = new GlobalConstants(error, maxDepth);
         
-        
+       
         // generate scene
+        //test_files\\scene6-alt
+        // submission_scenes\\scene4-specular
         Optional<File> file = Optional.empty();
-        String pathNameToFile = "/home/sauld/computer_programming/computing_graphics/HW3Java/test_files/scene3.test";
+        String pathNameToFile = "D:\\eclipse workspace\\3DRays-java\\submission_scenes\\scene7.test";
         file = Optional.of(new File(pathNameToFile));
         File fileToInsert = file.get();
         Scene fileScene = new Scene(fileToInsert);
         
-        String pathNameToOutFile = "/home/sauld/computer_programming/computing_graphics/HW3Java/my_images/output.png";
+        // set constants
+        int maxDepth = fileScene.maxDepth;
+        if (maxDepth == 0) {
+            maxDepth = 5;
+        }
+        
+        float error = 0.00001f;
+        GlobalConstants myConstants = new GlobalConstants(error, maxDepth);
+        
+        String pathNameToOutFile = "D:\\eclipse workspace\\3DRays-java\\my_image_files\\output.png";
         File outFile = new File(pathNameToOutFile);
         try {
             RenderImage.draw(fileScene, outFile);
